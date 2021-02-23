b7c6218898c0e37018bb50812fddfd9c02dd6bdd
matayyy <tesa12dinus>
2021-02-23 16:29:00 +0100

    new project from course on udemy

---
+++ src/test/java/project/matayyyy/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package project.matayyyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
